bcb5dbf5ae9646ed1c4961b36cb74a3803dcd5dc
“wangrunzhuang” <619410008@qq.com>
2018-07-25 13:09:49 +0800

    mm

---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="wrapper">
+    <video id="video"></video>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'landing-page',
+  data () {
+    return {}
+  },
+  mounted () {
+    this.video()
+  },
+  methods: {
+    video () {
+      navigator.mediaDevices
+        .getUserMedia()
+        .then(stream => {
+          console.log(stream)
+          /* 使用这个stream stream */
+        })
+        .catch(err => {
+          console.log(err)
+          /* 处理error */
+        })
+    }
+  }
+}
+</script>
